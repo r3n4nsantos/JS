19e4720ed949fdb699fc222921459a57a1703804
vai na web <vainawebcpx@gmail.com>
2019-11-01 12:15:24 -0300

    adicionei informacoes sobre todos do time

---
+++ main.js
@@ -0,0 +1,93 @@
+var lista = [
+    {
+        nome: Cicero,
+        idade: 24,
+        polo: Prazeres,
+    },
+    {
+        nome: Douglas,
+        idade: 24,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Priscila,
+        idade: 32,
+        polo: prazeres,
+    },
+    {
+        nome: Zaíra,
+        idade: 19,
+        polo: prazeres,
+    },
+    {
+        nome: Cassio,
+        idade: 21,
+        polo: prazeres,
+    },
+    {
+        nome: Dandara,
+        idade: 21,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Lucas,
+        idade: 24,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Big Jorge,
+        idade: 58,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Geisa,
+        idade: 19,
+        polo: Prazeres,
+    },
+    {
+        nome: Carol,
+        idade: 19,
+        polo: prazeres,
+    },
+    {
+        nome: stephania,
+        idade: 22,
+        polo: prazeres,
+    },
+    {
+        nome: Maicon,
+        idade: 23,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Cleiton,
+        idade: 19,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Rhuan,
+        idade: 20,
+        polo: Prazeres,
+    },
+    {
+        nome: Vitor,
+        idade: 24,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Arthur,
+        idade: 24,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Luan,
+        idade: 25,
+        polo: cpx do alemão,
+    },
+    {
+        nome: Renan/2n,
+        idade: 15,
+        polo: Prazeres,
+    },
+
+]    
